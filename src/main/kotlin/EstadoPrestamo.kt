929647cba31d7ba8b5ad8336f2d67b5a6f745517
PipetoBlack <fel.navarroc@hotmail.com>
2025-09-27 19:38:39 -0300

    Estructura base de estadoPrestamo

--- src/main/kotlin/EstadoPrestamo.kt
+++ src/main/kotlin/EstadoPrestamo.kt
@@ -1,4 +1,9 @@
 package org.example
 
-class EstadoPrestamo {
+// Una sealed class permite tener un conjunto cerrado de estados
+sealed class EstadoPrestamo {
+    object Pendiente : EstadoPrestamo()       // Cuando la solicitud recién se crea
+    object EnPrestamo : EstadoPrestamo()      // Cuando ya está activo el préstamo
+    object Devuelto : EstadoPrestamo()        // Cuando el libro fue devuelto
+    data class Error(val msg: String) : EstadoPrestamo() // Para manejar errores
 }
